442c4de32b46b19d4be2168e174d3ff318b0be20
romanmt <romanmt@gmail.com>
2012-02-14 11:10:56 -0500

    Game is working with start, steps and finish. Game engine is now a class and run game is recursive.
    converted game_engine from module to class
    
    made run_game recursive to handle multiple steps

--- lib/game_engine.rb
+++ lib/game_engine.rb
@@ -2,39 +2,54 @@ require 'singleton'
 class Game; include Singleton
   attr_reader :steps
 
+  def clear
+    @steps = {}
+  end
+ 
   def step(key, text, next_step)
-    @steps = {} if @steps.nil?   
     @steps[key] = {text: text, next: next_step}
   end
 end
 
-module GameEngine
-  def GameEngine.start(text, next_step)
+class GameEngine
+  def initialize(input, output)
+    @input = input
+    @output = output
+  end
+  
+  def start(text, next_step)
     Game.instance.step(:start, text, next_step)
   end
 
-  def GameEngine.step(key, text, next_step)
+  def step(key, text, next_step)
     Game.instance.step(key, text, next_step)
   end
 
-  def GameEngine.choose(key, text, *next_steps)
+  def choose(key, text, *next_steps)
     Game.instance.step(key, text, next_steps)
   end
 
-  def GameEngine.finish(key, text)
+  def finish(key, text)
     Game.instance.step(key, text, :finish)
   end
 
-  def GameEngine.load_game(file_name)
+  def load_game(file_name)
+    Game.instance.clear
     File.open(file_name) do |f|
-      module_eval(f.read)
+      instance_eval(f.read)
     end
   end
 
-  def GameEngine.run_game(input, output)
-    output.puts Game.instance.steps[:start][:text]
-    input.gets
-    output.puts Game.instance.steps[next_step(:start)][:text]
-    output.puts "Thanks for playing"
+  def run_game(step = :start)
+    @output.puts Game.instance.steps[step][:text]
+    if (nstep = next_step(step)) != :finish
+      @input.gets
+      run_game(nstep)
+    end
+  end
+
+private
+  def next_step(step)
+    Game.instance.steps[step][:next]
   end
 end
--- spec/game_engine_spec.rb
+++ spec/game_engine_spec.rb
@@ -8,65 +8,69 @@ def game_file(contents)
   file
 end
 
-def load_game(file_contents)
+def load_game(engine, file_contents)
   file = game_file(file_contents)
-  GameEngine.load_game(file)
+  engine.load_game(file)
   file.close; file.unlink
 end
 
 describe GameEngine do
+  let(:input) { double('input') }
+  let(:output) { double('output') }
+  let(:engine) { GameEngine.new(input, output) }
+
   describe '.run_game' do
-    context 'when the game has a start' do
-      it 'prints the start text' do
-        GameEngine.start('this is my game', :finish)
-        output = double('output')
-        output.should_receive(:puts).with('this is my game')
-        output.should_receive(:puts).with('Thanks for playing')
-        GameEngine.run_game(nil, output)
+    context 'that has start and finish' do
+      it 'waits for the user to hit enter' do 
+        load_game(engine, 'start "my game", :next
+                           finish :next, "whoo"')
+        output.should_receive(:puts).twice
+        input.should_receive(:gets)
+        engine.run_game
       end
     end
 
-    context 'when the game contains a next step' do
-      it 'waits for the user to hit enter' do 
-        GameEngine.start('my game', :next)
-        GameEngine.finish(:next, 'thanks for playing')
-        output = double('output')
-        output.should_receive(:puts).twice
-        input = double('input')
-        input.should_receive(:gets)
-        GameEngine.run_game(input, output)
+    context 'that has a start, step and finish' do
+      it 'waits for enter 2 times' do
+        load_game(engine, 'start "starting", :first_step
+                           step :first_step, "first step", :happy_ending
+                           finish :happy_ending, "Thanks for playing"')
+        output.should_receive(:puts).exactly(3).times
+        input.should_receive(:gets).twice
+        engine.run_game
       end
     end
+
   end
 
   describe '.load_game' do
     
-    context 'when the game has a start' do
+    context 'that has a start' do
       it 'stores the start and next step' do
-        load_game('start "starting game", :next_step')
+        load_game(engine, 'start "starting game", :next_step')
         Game.instance.steps[:start].should eql({text: "starting game", next: :next_step})
       end
     end
 
-    context 'when the game has a step' do
+    context 'that has a step' do
       it 'stores the step, text and next step' do
-        load_game('step :first, "the game is on", :second')
+        load_game(engine, 'step :first, "the game is on", :second')
         Game.instance.steps[:first].should eql({text: 'the game is on', next: :second})
       end
     end
 
-    context 'when the game has a choose' do
+    context 'that has a choose' do
       it 'stores the step, text and next step options' do
-        load_game('choose :diet_or_regular, "would you like diet or regular?", :diet, :regular')
+        load_game(engine, 'choose :diet_or_regular, "would you like diet or regular?", :diet, :regular')
         Game.instance.steps[:diet_or_regular].should eql(
           {text: 'would you like diet or regular?', next: [:diet, :regular]}
         )
       end
     end
 
-    context 'when the game has a finish' do
+    context 'that has a finish' do
       it 'stores the step, text and sets next step to :finish' do
-        load_game('finish :you_died, "You died, play again."')
+        load_game(engine, 'finish :you_died, "You died, play again."')
         Game.instance.steps[:you_died].should eql({text: "You died, play again.", next: :finish})
       end
     end
